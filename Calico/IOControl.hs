8ea13435ed8d68066b8d09d1ae8f996d83ab4b0a
Phil Ruffwind <rf@rufflewind.com>
2015-03-23 03:36:33 -0400

    Add more concurrency stuff

--- Calico/IOControl.hs
+++ Calico/IOControl.hs
@@ -1,16 +1,72 @@
 -- | This module is mutually exclusive with @Calico.MonadIOControl@.
+{-# LANGUAGE CPP #-}
 module Calico.IOControl (
     module Calico.IO
+
+    -- * Exceptions
   , catch
   , catchIOError
   , try
   , tryIOError
   , throwIO
+
+    -- * Concurrency
   , ThreadId
+  , myThreadId
   , forkIO
+  , forkIOWithUnmask
+  , forkFinally
+  , killThread
+  , throwTo
+  , forkOn
+  , forkOnWithUnmask
+
+    -- ** Scheduling
+  , getNumCapabilities
+  , setNumCapabilities
+  , threadCapability
+  , yield
+
+    -- ** Waiting
+  , threadDelay
+  , threadWaitRead
+  , threadWaitWrite
+
+    -- ** Bound threads
+  , rtsSupportsBoundThreads
+  , forkOS
+  , isCurrentThreadBound
+  , runInBoundThread
+  , runInUnboundThread
+
+    -- ** 'MVar'
+  , MVar
+  , newEmptyMVar
+  , newMVar
+  , takeMVar
+  , putMVar
+  , readMVar
+  , swapMVar
+  , tryTakeMVar
+  , tryPutMVar
+  , isEmptyMVar
+  , withMVar
+  , modifyMVar_
+  , modifyMVar
+
   ) where
 import Prelude ()
+#if !MIN_VERSION_base(4, 6, 0)
+import Calico.Base
+#endif
 import Calico.IO
 import Control.Concurrent
 import Control.Exception
 import System.IO.Error
+
+#if !MIN_VERSION_base(4, 6, 0)
+{-# INLINABLE forkFinally #-}
+forkFinally :: IO a -> (Either SomeException a -> IO ()) -> IO ThreadId
+forkFinally action finalAct = mask $ \ restore -> forkIO $
+  try (restore action) >>= finalAct
+#endif
--- Calico/MonadIOControl.hs
+++ Calico/MonadIOControl.hs
@@ -1,15 +1,60 @@
-{-# LANGUAGE FlexibleContexts #-}
 -- | This module is mutually exclusive with @Calico.IOControl@.
+{-# LANGUAGE CPP, FlexibleContexts #-}
 module Calico.MonadIOControl (
     module Calico.MonadIO
   , MonadBaseControl(..)
+
+    -- * Exceptions
   , catch
   , catchIOError
   , try
   , tryIOError
   , throwIO
+
+    -- * Concurrency
   , ThreadId
+  , myThreadId
   , fork
+  , forkWithUnmask
+  , forkFinally
+  , killThread
+  , throwTo
+  , forkOn
+  , forkOnWithUnmask
+
+    -- ** Scheduling
+  , getNumCapabilities
+  , setNumCapabilities
+  , threadCapability
+  , yield
+
+    -- ** Waiting
+  , threadDelay
+  , threadWaitRead
+  , threadWaitWrite
+
+    -- ** Bound threads
+  , rtsSupportsBoundThreads
+  , forkOS
+  , isCurrentThreadBound
+  , runInBoundThread
+  , runInUnboundThread
+
+    -- ** 'MVar'
+  , MVar
+  , newEmptyMVar
+  , newMVar
+  , takeMVar
+  , putMVar
+  , readMVar
+  , swapMVar
+  , tryTakeMVar
+  , tryPutMVar
+  , isEmptyMVar
+  , withMVar
+  , modifyMVar_
+  , modifyMVar
+
   ) where
 import Prelude ()
 import Calico.Base
@@ -18,8 +63,18 @@ import Control.Concurrent.Lifted
 import Control.Exception.Lifted
 import Control.Monad.Trans.Control (MonadBaseControl(..))
 
+{-# INLINABLE catchIOError #-}
 catchIOError :: MonadBaseControl IO m => m a -> (IOError -> m a) -> m a
 catchIOError = catch
 
+{-# INLINABLE tryIOError #-}
 tryIOError :: MonadBaseControl IO m => m a -> m (Either IOError a)
 tryIOError = try
+
+#if !(MIN_VERSION_base(4, 6, 0) && MIN_VERSION_lifted_base(0, 2, 0))
+{-# INLINABLE forkFinally #-}
+forkFinally :: MonadBaseControl IO m =>
+               m a -> (Either SomeException a -> m ()) -> m ThreadId
+forkFinally action finalAct = mask $ \ restore -> fork $
+  try (restore action) >>= finalAct
+#endif
